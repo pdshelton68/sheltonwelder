c946c7ad5f83e54325aa46c88ca974174c9de391
Paul Shelton <paul.shelton@gmail.com>
2019-12-29 16:22:13 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+console.log("dir name " + __dirname);
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist/sheltonwelder'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/sheltonwelder/index.html'));
+  });
+
+app.listen(process.env.PORT || 8080);
+
