16b4e63729fb9a18bb2448703e80b203a04e99f2
Paul Shelton <paul.shelton@gmail.com>
2020-01-03 17:44:11 -0600

    initial commit

--- src/rsvp-mobile/rsvp-mobile.component.ts
+++ src/rsvp-mobile/rsvp-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MailService } from '../services/mailservice';
 
 @Component({
   selector: 'app-rsvp-mobile',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rsvp-mobile.component.css']
 })
 export class RsvpMobileComponent implements OnInit {
-  public state:string = 'rsvp';
-  constructor() { }
+  public yesNo:number= 1;
+  public name: string ="";
+  public numGuests:number = 2;
+  public email:string = "";
+
+  public state:string = "rsvp";
+
+  constructor(private service:MailService) { }
 
   ngOnInit() {
   }
 
+
+
+  public validForm():boolean{
+    return this.name.length >0 && this.email.length>0;
+  }
+
+  public onSendMail(){
+    if(!this.validForm()) return;
+    
+    this.state = "submit";
+    this.service.sendMail(this.email, this.yesNo > 0, "",this.numGuests, this.name).subscribe(data=>{
+      this.state = "complete";
+      console.log("ok");
+    })
+
+  }
+
+  public reset(){
+    this.name = "";
+    this.email = "";
+    this.numGuests = 2;
+    this.yesNo = 1;
+  }
 }
